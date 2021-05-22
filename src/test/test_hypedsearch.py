e49b6c9a3e9bf14c662d0feb1a7c7ddf3e41f20e
Jamie Dixon <jamessdixon@gmail.com>
2021-05-22 09:17:23 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -2,10 +2,10 @@ import unittest
 
 class TestSum():
 
-    def test_add_pass():
+    def test_add_pass(self):
         self.assertEqual(1+1, 2, "Should be 2")
 
-    def test_add_fail():
+    def test_add_fail(self):
         self.assertEqual(1+1, 1, "Should be 2")
     
 if __name__ == '__main__':
