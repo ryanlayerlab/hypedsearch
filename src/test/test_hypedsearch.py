faea70215d5c7cc644a7cdcffe7bd980fc7caf9b
Jamie Dixon <jamessdixon@gmail.com>
2021-05-22 09:09:06 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -1,1 +1,12 @@
-#first unit test here
+import hypedsearch
+
+def test_add_pass():
+    assert 1+1 == 2, "Should be 2"
+
+def test_add_fail():
+    assert 1+1 == 1, "Should be 2"
+
+if __name__ == "__main__":
+    test_add_pass()
+    test_add_fail()
+    print("All tests passed")
