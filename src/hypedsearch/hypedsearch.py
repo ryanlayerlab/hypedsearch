eafa8118f94787190e1d8d4d1268f9bfa35907d3
Jamie Dixon <jamessdixon@gmail.com>
2021-05-30 09:11:45 -0600

    Update hypedsearch.py

--- src/hypedsearch/hypedsearch.py
+++ src/hypedsearch/hypedsearch.py
@@ -1,12 +1,6 @@
 import sys
 import argparse
 
-def string_to_bool(s: str) -> bool:
-    s = str(s)
-    if s.lower() == 'false' or 'f' in s.lower():
-        return False
-    return True
-
 def main():
     print("Hello World!")
 
