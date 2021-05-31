49ad3b309b85ca2f82f1651b4405ad11bcbfbc04
Jamie Dixon <jamessdixon@gmail.com>
2021-05-31 17:27:48 -0600

    Update hypedsearch.py

--- src/hypedsearch/hypedsearch.py
+++ src/hypedsearch/hypedsearch.py
@@ -7,20 +7,20 @@ from dataclasses import dataclass
 @dataclass
 class Main_Arguments:
     spectra_folder: str = ''
-    #database_file: str
-    #output_dir: str
-    #min_peptide_len: int
-    #max_peptide_len: int
-    #tolerance: float
-    #precursor_tolerance: float
-    #verbose: str
-    #peak_filter: float
-    #relative_abundance_filter: float
-    #digest: str
-    #DEBUG: bool
-    #cores: int
-    #n: int
-    #truth_set: str
+    database_file: str = ''
+    output_dir: str = ''
+    min_peptide_len: int = 0
+    max_peptide_len: int = 0
+    tolerance: float = 0.0
+    precursor_tolerance: float = 0.0
+    verbose: str = ''
+    peak_filter: float = 0.0
+    relative_abundance_filter: float = 0.0
+    digest: str = ''
+    DEBUG: bool = True
+    cores: int = 0
+    n: int = 0
+    truth_set: str = ''
 
 def main(args: Main_Arguments) -> None:
     print("Hello World!")
