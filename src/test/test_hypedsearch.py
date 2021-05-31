0672ab4df6adfa8ce702d0da972d8142bc41b966
Jamie Dixon <jamessdixon@gmail.com>
2021-05-31 17:08:54 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -1,14 +1,25 @@
-import unittest
+#import unittest
 
 #Fails
-import hypedsearch
+#import hypedsearch
 #hypedsearch = __import__("../hypedsearch/hypedsearch.py")
 #hypedsearch = __import__("hypedsearch.py")
 
-class TestSum(unittest.TestCase):
+
+import unittest
+import sys    
+
+class Test_Main(unittest.TestCase):
+
+    def setUp(self):
+        sys.path.insert(0, "../project")
+        import hypedsearch
+        #from project import item
+        # further setup using this import
 
     def test_add_pass(self):
         self.assertEqual(2, 2, "Should pass")
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
+
