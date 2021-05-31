c7435c24a56de615f690056173d89c4bcc5602e6
Jamie Dixon <jamessdixon@gmail.com>
2021-05-31 17:40:35 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -10,12 +10,9 @@ class Test_Main(unittest.TestCase):
         import hypedsearch
         self.main_arguments = hypedsearch.Main_Arguments()
         
-    def test_add_pass(self):
-        self.assertEqual(2, 2, "Should pass")
-
-    def test_main_pass(self):
+    def test_main_arguments_specta_folder(self):
         self.main_arguments.spectra_folder = 'C:\MyFolder'
-        self.assertEqual(self.main_arguments.spectra_folder, 'C:\MyFolder2', 'spectra_folder is incorrect')      
+        self.assertEqual(self.main_arguments.spectra_folder, 'C:\MyFolder', 'spectra_folder is incorrect')      
         
 if __name__ == "__main__":
     unittest.main()
