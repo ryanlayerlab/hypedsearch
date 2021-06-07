3ccbbe10b931d8f4d0a7319c61939745bb1660bb
Jamie Dixon <jamessdixon@gmail.com>
2021-06-07 08:42:09 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -8,8 +8,8 @@ class Test_Main(unittest.TestCase):
     def setUp(self):
         sys.path.insert(0, "../hypedsearch")
         import hypedsearch
-        #args = {min_peptide_len=1}
-        hypedsearch.main()
+        args = "foo"
+        hypedsearch.main(args)
         
     def test_main_arguments_specta_folder(self):
         self.assertEqual(1,1,'equality test')
