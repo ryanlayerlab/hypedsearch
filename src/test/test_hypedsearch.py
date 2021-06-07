7d48ea740412266877e9526b79ab8545265c2245
Jamie Dixon <jamessdixon@gmail.com>
2021-06-07 08:42:21 -0600

    Update test_hypedsearch.py

--- src/test/test_hypedsearch.py
+++ src/test/test_hypedsearch.py
@@ -8,7 +8,7 @@ class Test_Main(unittest.TestCase):
     def setUp(self):
         sys.path.insert(0, "../hypedsearch")
         import hypedsearch
-        args = "foo"
+        args = 'foo'
         hypedsearch.main(args)
         
     def test_main_arguments_specta_folder(self):
