c522f6a2a2847b43736c4693482dfccba8c1c72d
Jamie Dixon <jamessdixon@gmail.com>
2021-06-15 20:25:50 -0600

    added arg class to id_spectra

--- src/hypedsearch/runner.py
+++ src/hypedsearch/runner.py
@@ -1,31 +1,11 @@
 import os
+import utils
 import identification
-import multiprocessing as mp
 from postprocessing import summary, review
 
 def run(args: dict) -> None:
-    spectra_files = []
-    for (root, _, filenames) in os.walk(args['spectra_folder']):
-        for fname in filenames:
-            spectra_files.append(os.path.join(root, fname))
-    cores = max(1, args['cores'])
-    cores = min(cores, mp.cpu_count() - 1)
-    matched_spectra = identification.id_spectra(
-        spectra_files, args['database_file'], 
-        min_peptide_len=args['min_peptide_len'], 
-        max_peptide_len=args['max_peptide_len'], 
-        ppm_tolerance=args['tolerance'], 
-        precursor_tolerance=args['precursor_tolerance'],
-        peak_filter=args['peak_filter'],
-        relative_abundance_filter=args['relative_abundance_filter'],
-        digest=args['digest'], 
-        n=args['n'] * 10,
-        verbose=True, 
-        is_debug=args['DEBUG'], 
-        cores=cores,
-        truth_set=args['truth_set'], 
-        output_dir=args['output_dir']
-    )
+    id_spectra_arguments = utils.create_id_spectra_arguments(args)
+    matched_spectra = identification.id_spectra(id_spectra_arguments)
     print('\nFinished search. Writting results to {}...'.format(args['output_dir']))
     summary.generate(matched_spectra, args['output_dir'])
     
