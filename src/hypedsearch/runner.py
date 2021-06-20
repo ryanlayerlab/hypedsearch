813b4fae7d917885e51696df87c2631679efa0d0
Jamie Dixon <jamessdixon@gmail.com>
2021-06-19 18:58:33 -0600

    renamed things

--- src/hypedsearch/runner.py
+++ src/hypedsearch/runner.py
@@ -7,6 +7,7 @@ def run(args: dict) -> None:
     id_spectra_arguments = utils.create_id_spectra_arguments(args)
     matched_spectra = identification.id_spectra(id_spectra_arguments)
     print('\nFinished search. Writting results to {}...'.format(args['output_dir']))
-    summary.generate(matched_spectra, args['output_dir'])
+    #output_dir = args['output_dir']
+    #summary.generate(matched_spectra, output_dir)
     
     
