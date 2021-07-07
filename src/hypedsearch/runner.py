3bf4e79301fd23b4ccc2ddb6be61e028b66bf50f
Jamie Dixon <jamessdixon@gmail.com>
2021-07-07 05:28:06 -0600

    pulled out database

--- src/hypedsearch/runner.py
+++ src/hypedsearch/runner.py
@@ -2,27 +2,13 @@ import os
 import identification
 from postprocessing import summary, review
 import multiprocessing as mp
-from objects import Database
-import database
 
-def get_spectra_files(args: dict):
-    spectra_files = []
-    for (root, _, filenames) in os.walk(args['spectra_folder']):
-        for fname in filenames:
-            spectra_files.append(os.path.join(root, fname))
-    return spectra_files
-
-def get_database_file(args: dict):
-    database_file_path = args['database_file']
-    return database.build(database_file_path)
 
 def run(args: dict) -> None:
-    spectra_files = get_spectra_files(args)
-    database_file = get_database_file(args)
     cores = max(1, args['cores'])
     cores = min(cores, mp.cpu_count() - 1)
     matched_spectra = identification.id_spectra(
-        spectra_files, database_file, 
+        args['spectra_files'], args['database_file'], 
         min_peptide_len=args['min_peptide_len'], 
         max_peptide_len=args['max_peptide_len'], 
         ppm_tolerance=args['tolerance'], 
