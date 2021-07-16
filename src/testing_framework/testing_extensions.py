e0d5285f35f3861e4f50981b9b722c101c753927
NCollins0481 <ncol107453@gmail.com>
2021-07-16 18:12:27 -0300

    Cleaned up testing_code

--- src/testing_framework/testing_extensions.py
+++ src/testing_framework/testing_extensions.py
@@ -51,44 +51,9 @@ db = database.build(dataset[2])
 matched_masses_b, matched_masses_y, db = testing_utils.modified_match_masses(boundaries, db, max_peptide_length)
 
 print('Starting mapping...')
-mz_mapping = defaultdict(set)
-for spectrum in input_spectra:
-    for i, mz in enumerate(spectrum[0]):
-        boundaries = preprocessing_utils.make_boundaries(mz, ppm_tolerance)
-        for boundary in matched_masses_b.keys():
-            if boundary == str(boundaries[0]) + '-' + str(boundaries[1]):
-                for matching in matched_masses_b[boundary]:
-                    mz_mapping[mz].add((mz, matching[0], i, matching[2], matching[1], matching[3], matching[4]))
-        for boundary in matched_masses_y.keys():
-            if boundary == str(boundaries[0]) + '-' + str(boundaries[1]):
-                for matching in matched_masses_b[boundary]:
-                    mz_mapping[mz].add((mz, matching[0], i, matching[2], matching[1], matching[3], matching[4]))
-
+testing_utils.map_mz
+mz_mapping = testing_utils.map_mz(input_spectra, ppm_tolerance, matched_masses_b, matched_masses_y)
 print('Done')
+
 mz_mapping = sorted(mz_mapping)
 print(mz_mapping)
-
-
-
-
-print('I want dinner...')
-
-
-
-
-
-
-
-
-#Length is 3867 for b and 3793 for y after changes
-#Length is 4000 for b and 3927 for y after changes
-
-#Map to (P_y, S_i, P_j, seq, b/y)
-# Where P_y is protein this was found in, S_i is m/z number, P_j is location within that protein, seq and b/y are straightforward
-
-# Find initial hits
-
-# for i, mz in enumerate(input_spectrum):
-#     #Map to (P_y, S_i, P_j, seq, b/y)
-#     # Where P_y is protein this was found in, S_i is m/z number, P_j is location within that protein, seq and b/y are straightforward
-#     for prot in proteins
