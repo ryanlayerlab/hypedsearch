59f4ff35b5465793632950c1f7e5b3fe2155fd52
jamesdixon <jamessdixon@gmail.com>
2021-08-26 19:06:18 -0600

    refactored out more methods

--- src/identification.py
+++ src/identification.py
@@ -27,20 +27,39 @@ filter_times = []
 
 TOP_X = 50
 
+def handle_DEV_truth(filtered_b,filtered_y,b_results,keep_b_count,y_results,keep_y_count,fall_off,_id,is_hybrid,truth_seq,spectrum):
+    metadata = {
+        'top_x_b_hits': filtered_b, 
+        'top_x_y_hits': filtered_y, 
+        'excluded_b_hits': [x[0] for x in b_results[keep_b_count:]],
+        'excluded_y_hits': [x[0] for x in y_results[keep_y_count:]], 
+        'cut_off_b_score': b_results[keep_b_count - 1][1], 
+        'cut_off_y_score': y_results[keep_y_count - 1][1]
+    }
+    fall_off[_id] = DEVFallOffEntry(
+        is_hybrid, 
+        truth_seq, 
+        'top_x_filtering', 
+        metadata
+    )
+    
+def adjust_for_truth_and_fall_off(spectrum,truth,filtered_b,filtered_y,b_results,keep_b_count,y_results,keep_y_count,fall_off):
+    _id = spectrum.id
+    truth_seq = truth[_id]['sequence']
+    is_hybrid = truth[_id]['hybrid']
+    if not utils.DEV_contains_truth_parts(truth_seq, is_hybrid, filtered_b, filtered_y):
+        handle_DEV_truth(filtered_b,filtered_y,b_results,keep_b_count,y_results,keep_y_count,fall_off,_id,is_hybrid,truth_seq,spectrum)
+        return Alignments(spectrum, [])
+
 def id_spectrum(spectrum: Spectrum, db: Database, b_hits: dict, y_hits: dict,
     ppm_tolerance: int, precursor_tolerance: int,n: int,digest_type: str = '',
     truth: dict = None, fall_off: dict = None, is_last: bool = False):
 
     precursor_tolerance = utils.ppm_to_da(spectrum.precursor_mass, precursor_tolerance)
-    score_b_start = time.time()
     b_intermediate = [(kmer, mass_comparisons.optimized_compare_masses(spectrum.mz_values, gen_spectra.gen_spectrum(kmer, ion='b'))) for kmer in b_hits]
     b_results = sorted(b_intermediate, key=lambda x: (x[1], 1/len(x[0])), reverse=True)
-    b_scoring_times.append(time.time() - score_b_start)
-    score_y_start = time.time()
     y_intermediate = [(kmer, mass_comparisons.optimized_compare_masses(spectrum.mz_values, gen_spectra.gen_spectrum(kmer, ion='y'))) for kmer in y_hits]
     y_results = sorted(y_intermediate, key=lambda x: (x[1], 1/len(x[0])), reverse=True)
-    y_scoring_times.append(time.time() - score_y_start)
-    filter_start = time.time()
     filtered_b, filtered_y = [], []
     max_b_score = max([x[1] for x in b_results])
     max_y_score = max([x[1] for x in y_results])
@@ -50,34 +69,13 @@ def id_spectrum(spectrum: Spectrum, db: Database, b_hits: dict, y_hits: dict,
     keep_y_count = max(TOP_X, num_max_y)
     filtered_b = [x[0] for x in b_results[:keep_b_count] if x[1] > 0]
     filtered_y = [x[0] for x in y_results[:keep_y_count] if x[1] > 0]
-    filter_times.append(time.time() - filter_start)
     if truth is not None and fall_off is not None:
-        _id = spectrum.id
-        truth_seq = truth[_id]['sequence']
-        is_hybrid = truth[_id]['hybrid']
-        if not utils.DEV_contains_truth_parts(truth_seq, is_hybrid, filtered_b, filtered_y):
-            metadata = {
-                'top_x_b_hits': filtered_b, 
-                'top_x_y_hits': filtered_y, 
-                'excluded_b_hits': [x[0] for x in b_results[keep_b_count:]],
-                'excluded_y_hits': [x[0] for x in y_results[keep_y_count:]], 
-                'cut_off_b_score': b_results[keep_b_count - 1][1], 
-                'cut_off_y_score': y_results[keep_y_count - 1][1]
-            }
-            fall_off[_id] = DEVFallOffEntry(
-                is_hybrid, 
-                truth_seq, 
-                'top_x_filtering', 
-                metadata
-            )
-            return Alignments(spectrum, [])
-
-    align_start = time.time()
+        early_return = adjust_for_truth_and_fall_off(spectrum,truth,filtered_b,filtered_y,b_results,keep_b_count,y_results,keep_y_count,fall_off)
+        if early_return is not None:
+            return early_return        
     alignments = alignment.attempt_alignment(spectrum, db, filtered_b, filtered_y, 
         ppm_tolerance=ppm_tolerance, precursor_tolerance=precursor_tolerance,
         n=n, truth=truth, fall_off=fall_off, is_last=is_last)
-    TOT_ALIGNMENT = time.time() - align_start
-    alignment_times.append(TOT_ALIGNMENT)
     return alignments
 
 def mp_id_spectrum(input_q: mp.Queue, db_copy: Database, results: dict, fall_off: dict = None, truth: dict = None):
@@ -221,7 +219,6 @@ def id_spectra(spectra_files: list, db: database, verbose: bool = True,
     verbose and print('Loading database Done')
     verbose and print('Loading spectra...')
     spectra, boundaries, mz_mapping = preprocessing_utils.load_spectra(spectra_files, ppm_tolerance, peak_filter=peak_filter, relative_abundance_filter=relative_abundance_filter)
-    
     verbose and print('Loading spectra Done')
     matched_masses_b, matched_masses_y, kmer_set = merge_search.match_masses(boundaries, db, max_peptide_len)
     db = db._replace(kmers=kmer_set)
