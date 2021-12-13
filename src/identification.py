f5dcd9212bbbec53cd901e08bd3f7cafd66b9ffc
NCollins0481 <ncol107453@gmail.com>
2021-12-13 09:54:10 -0700

    Normalized precursor mass to double charge, extended kmer length to 23 (though not optimized)

--- src/identification.py
+++ src/identification.py
@@ -193,7 +193,7 @@ def handle_DEV_result(output_dir,fall_off,cores):
         )          
 
 def id_spectra(spectra_files: list, db: database, verbose: bool = True, 
-    min_peptide_len: int = 5, max_peptide_len: int = 20, peak_filter: int = 0, 
+    min_peptide_len: int = 5, max_peptide_len: int = 23, peak_filter: int = 0, 
     relative_abundance_filter: float = 0.0,ppm_tolerance: int = 20, 
     precursor_tolerance: int = 10, digest: str = '',cores: int = 1,
     n: int = 5,DEBUG: bool = False, truth_set: str = '', output_dir: str = ''):
--- src/preprocessing/preprocessing_utils.py
+++ src/preprocessing/preprocessing_utils.py
@@ -1,11 +1,22 @@
 from file_io import spectra
 from utils import ppm_to_da
+from constants import DOUBLY_CHARGED_B_BASE, DOUBLY_CHARGED_Y_BASE, PROTON_MASS, WATER_MASS
 
 # turn the all spectra list into a list of boundaries
 def make_boundaries(mz, ppm_tol):
     da_tol = ppm_to_da(mz, ppm_tol)
     return [mz - da_tol, mz + da_tol]
 
+def get_total_sum(m,z):
+    total_sum = 0
+    total_sum = (m * z) - (z * PROTON_MASS) - WATER_MASS
+    return total_sum
+def normalize_to_two(m, z):
+    total_sum = get_total_sum(m, z)
+    normalized_b = (DOUBLY_CHARGED_B_BASE + total_sum) / 2
+    normalized_y = (DOUBLY_CHARGED_Y_BASE + total_sum) / 2
+    return normalized_b, normalized_y
+    
 def load_spectra(
     spectra_files: list, ppm_tol: int, peak_filter: int = 0, relative_abundance_filter: float = 0.0):
     linear_spectra = []
@@ -17,8 +28,11 @@ def load_spectra(
             relative_abundance_filter=relative_abundance_filter
         )
         for object in these_spectra:
-            object.mz_values.append(object.precursor_mass * object.precursor_charge)
+            b_prec, y_prec = normalize_to_two(object.precursor_mass, object.precursor_charge)
+            object.mz_values.append(b_prec)
+            object.mz_values.append(y_prec)
             object.abundance.append(100000000) #I gave it a crazy high abundance to represent precursor. Still a hack
+            object.abundance.append(100000000) 
         all_spectra += these_spectra
         # these_spectra = [these_spectra[0]]
         # all_spectra = [all_spectra[0]]
