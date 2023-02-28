d1f999336bf316e1df273e9c0f8ae4912c450a40
Kaylee Rosenberger <karo9276@mendel.colorado.edu>
2023-02-28 16:15:00 -0700

    pushing most recent guessing_hybrids script

--- src/Guessing_hybrids.py
+++ src/Guessing_hybrids.py
@@ -12,7 +12,7 @@ import random
 
 #These are user inputted parameters and their typical values
 ppm_tolerance = 20 
-peak_filter = 100 #given 25 peaks as information, some other software uses 50-100
+peak_filter = 25 #given 25 peaks as information, some other software uses 50-100
 relative_abundance_filter = .1
 prec_tol = 10
 max_pep_len = 25 
@@ -28,12 +28,9 @@ def first_pass_truth_set(filepath):
 
     return correct_sequences
 
-def find_overlaps(sequence, ppm_tol, input_masses):
+def find_overlaps(masses, ppm_tol, input_masses):
     total_score = 0
     overlap_masses = list()
-    spectrum = gen_spectra.gen_spectrum(sequence)
-    masses = sorted(spectrum['spectrum'])
-    input_masses = sorted(input_masses)
     o_ctr, t_ctr = 0, 0
     observed = input_masses[o_ctr]
     theoretical = masses[t_ctr]
@@ -78,65 +75,63 @@ spectra = preprocessing_utils.load_spectra(spectra_file, ppm_tolerance, peak_fil
 truth_set_path = '/home/karo9276/HypedSearch/hypedsearch/data/truth_table/NOD2_E3_results.ssv'
 specmill_seqs = first_pass_truth_set(truth_set_path)
 
-#See which specmill seqs have len < 25
-ctr = 0
-for seq in specmill_seqs:
-    if(len(seq) >= 25):
-        ctr = ctr + 1
-
 #In this dataset, these are known hybrids (added manually)
-#hybrid_seqs = specmill_seqs[4:11]
+hybrid_seqs = specmill_seqs[4:11]
 #everything else in the dataset is a natural 
 
 # #container stores result of if a spectrum is hybrid or not 
-# is_hybrid = list()
+is_hybrid = list()
+is_hybrid_seq = list()
 
 # #container to store number of precursor matches for each spectrum
-# b_precursor_matches = list()
-# y_precursor_matches = list()
+b_precursor_matches = list()
+y_precursor_matches = list()
 
-# #This loop checks each spectrum to determine if it is a hybrid peptide
-# for i,spectrum in enumerate(spectra):
-#     #This matches every mz in a spectrum with a list of kmers it can match to. Format is (m/z, location_start, location_end, ion, charge, parent_protein)
-#     b_prec, y_prec = gen_spectra.convert_precursor_to_ion(spectrum.precursor_mass, spectrum.precursor_charge)
-#     matched_masses_b, matched_masses_y = merge_search.modified_match_masses(spectrum.mz_values, proteins, max_pep_len, ppm_tolerance, b_prec, y_prec)
+""" # #This loop checks each spectrum to determine if it is a hybrid peptide
+for i,spectrum in enumerate(spectra):
+    #This matches every mz in a spectrum with a list of kmers it can match to. Format is (m/z, location_start, location_end, ion, charge, parent_protein)
+    b_prec, y_prec = gen_spectra.convert_precursor_to_ion(spectrum.precursor_mass, spectrum.precursor_charge)
+    matched_masses_b, matched_masses_y = merge_search.modified_match_masses(spectrum.mz_values, proteins, max_pep_len, ppm_tolerance, b_prec, y_prec)
 
-#     #FIRST ROUTE TO CHECK IF HYBRID - CHECK PRECURSOR WEIGHT MATCHES 
-#     #Get precursor mass
-#     b_precursor, y_precursor = list(matched_masses_b.keys())[-2], list(matched_masses_y.keys())[-1]
+    #FIRST ROUTE TO CHECK IF HYBRID - CHECK PRECURSOR WEIGHT MATCHES 
+#    #Get precursor mass
+    b_precursor, y_precursor = list(matched_masses_b.keys())[-2], list(matched_masses_y.keys())[-1]
 
-#     #Getting everything that matched the precursor weights
-#     all_b_hits, all_y_hits = matched_masses_b[b_precursor], matched_masses_y[y_precursor]
+#   #Getting everything that matched the precursor weights
+    all_b_hits, all_y_hits = matched_masses_b[b_precursor], matched_masses_y[y_precursor]
 
-#     #save number of precursor match hits
-#     b_precursor_matches.append(len(all_b_hits))
-#     y_precursor_matches.append(len(all_y_hits))
+#    #save number of precursor match hits
+    b_precursor_matches.append(len(all_b_hits))
+    y_precursor_matches.append(len(all_y_hits))
     
-#     #Loop over all precursor weight hits and score each one 
-#     b_scores, y_scores = list(), list()
-#     seq_set = set()
-#     for hit in all_b_hits: 
-#         #Obtain sequence from hit 
-#         seq = preprocessing_utils.find_sequence(hit, proteins.proteins)
-#         seq_set.add(seq)
-#         #Score the hit - score = number of peaks matched (out of 25)
-#         b_scores.append(overlap_scoring(seq, ppm_tolerance, spectrum.mz_values))
-#     for hit in all_y_hits: 
-#         seq = preprocessing_utils.find_sequence(hit, proteins.proteins)
-#         seq_set.add(seq)
-#         y_scores.append(overlap_scoring(seq, ppm_tolerance, spectrum.mz_values))
-    
-#     #If SpecMill sequence is found in our precursor match sequences anywhere, not hybrid? 
-#     #What if seq_set is empty?
-#     count = 0
-#     if(specmill_seqs[i] in seq_set):
-#         count = count + 1  
+#    #Loop over all precursor weight hits and score each one 
+    b_scores, y_scores = list(), list()
+    seq_set = set()
+    for hit in all_b_hits: 
+#        #Obtain sequence from hit 
+        seq = preprocessing_utils.find_sequence(hit, proteins.proteins)
+        seq_set.add(seq)
+        #Score the hit - score = number of peaks matched (out of 25)
+        #b_scores.append(overlap_scoring(seq, ppm_tolerance, spectrum.mz_values))
+    for hit in all_y_hits: 
+        seq = preprocessing_utils.find_sequence(hit, proteins.proteins)
+        seq_set.add(seq)
+        #y_scores.append(overlap_scoring(seq, ppm_tolerance, spectrum.mz_values))
+         #If SpecMill sequence is found in our precursor match sequences anywhere, not hybrid? 
+    #What if seq_set is empty?
+    count = 0
+    if(specmill_seqs[i] in seq_set):
+        count = count + 1  
 
-#     if(count == 0 ): 
-#         is_hybrid.append(1)
-#     else: 
-#         is_hybrid.append(0)
+    if(count == 0 ): 
+        is_hybrid.append(1)
+    else: 
+        is_hybrid.append(0)
         
+    if(len(seq_set) == 0 & len(specmill_seqs[i]) >= 25):
+        is_hybrid_seq.append(1)
+    else: 
+        is_hybrid_seq.append(0)
 #     #Perhaps if the mean scores are below some threshold, they are hybrid? 
 #         #Need to truth this with some instances 
 
@@ -144,7 +139,7 @@ for seq in specmill_seqs:
 #     #Best scoring, highest mass match (with charge accounted for...)
 #     #How does it compare to the precursor weight? 
 #     #If there is a mass close to the precursor with a 'high' score, it may not be hybrid 
-
+ """
 # print("The number of potential hybrids is: ")
 # print(sum(is_hybrid))
 # print(" out of: ")
@@ -172,118 +167,193 @@ known_naturals = spectra[:4] + spectra[12:]
 known_hybrid_seqs = specmill_seqs[4:11]
 known_natural_seqs = specmill_seqs[:4] + specmill_seqs[12:]
 
-def rand_jitter(arr):
-    stdev = .01 * (max(arr) - min(arr))
-    random_list = list()
-    for element in arr:
-        random_num = random.randrange(0, 1000)
-        random_list.append(random_num)
-    return random_list 
-
 #Hybrids 
 all_overlap_masses_hyb = list()
+biggest_b_hit_hyb = list()
+biggest_y_hit_hyb = list()
 all_yaxis = list()
 scores_hyb = list() 
 for i,hybrid in enumerate(known_hybrids):
     hybrid_seq = known_hybrid_seqs[i]
     if len(hybrid_seq) <= 25:
-        score, overlap_masses = find_overlaps(hybrid_seq, ppm_tolerance, hybrid.mz_values)
-        [all_overlap_masses_hyb.append(x) for x in overlap_masses] #/len(hybrid_seq)
-        y_axis = rand_jitter(overlap_masses)
-        [all_yaxis.append(x) for x in y_axis]
-        [scores_hyb.append(score) for x in overlap_masses]
-    
+        spectrum_b_1 = gen_spectra.gen_spectrum(hybrid_seq, 1, 'b')
+        spectrum_b_2 = gen_spectra.gen_spectrum(hybrid_seq, 2, 'b')
+        spectrum_y_1 = gen_spectra.gen_spectrum(hybrid_seq, 1, 'y')
+        spectrum_y_2 = gen_spectra.gen_spectrum(hybrid_seq, 2, 'y')
+        score_b1, overlap_masses_b1 = find_overlaps(spectrum_b_1, ppm_tolerance, hybrid.mz_values)
+        score_b2, overlap_masses_b2 = find_overlaps(spectrum_b_2, ppm_tolerance, hybrid.mz_values)
+        score_y1, overlap_masses_y1 = find_overlaps(spectrum_y_1, ppm_tolerance, hybrid.mz_values)
+        score_y2, overlap_masses_y2 = find_overlaps(spectrum_y_2, ppm_tolerance, hybrid.mz_values)
+        
+        converted_masses_b1 = list()
+        converted_masses_b2 = list()
+        converted_masses_y1 = list()
+        converted_masses_y2 = list()
+        for mass in overlap_masses_b1:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 0, 1, hybrid.precursor_charge)
+            converted_masses_b1.append(converted_mass)
+        for mass in overlap_masses_b2:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 0, 2, hybrid.precursor_charge)
+            converted_masses_b2.append(converted_mass)
+        for mass in overlap_masses_y1:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 1, 1, hybrid.precursor_charge)
+            converted_masses_y1.append(converted_mass)
+        for mass in overlap_masses_y2:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 1, 2, hybrid.precursor_charge)
+            converted_masses_y2.append(converted_mass)
+        
+         #Get the biggest hit for b and y ions 
+        biggest_b_hit = max(converted_masses_b1 + converted_masses_b2) #biggest b hit 
+        biggest_y_hit = max(converted_masses_y1 + converted_masses_y2)
+        
+        #Divide by the precursor mass 
+        hyb_prec = hybrid.precursor_mass
+        biggest_b_hit_hyb.append(biggest_b_hit/hyb_prec)
+        biggest_y_hit_hyb.append(biggest_y_hit/hyb_prec)
+        scores_hyb.append(score_b1 + score_b2 + score_y1 + score_y2)
+             
 #Naturals 
 all_overlap_masses_nat = list()
+biggest_b_hit_nat = list()
+biggest_y_hit_nat = list()
 all_yaxis_nat = list()
 scores_nat = list()
 for i,natural in enumerate(known_naturals):
     natural_seq = known_natural_seqs[i]
     if len(natural_seq) <= 25:
-        score, overlap_masses = find_overlaps(natural_seq, ppm_tolerance, natural.mz_values)
-        [all_overlap_masses_nat.append(x) for x in overlap_masses] #/len(natural_seq)
-        if len(overlap_masses) != 0: 
-            y_axis = rand_jitter(overlap_masses)
-            [all_yaxis_nat.append(x) for x in y_axis]
-        [scores_nat.append(score) for x in overlap_masses]
+        #total_score = 0
+        #overlap_masses = list()
+        spectrum_b_1 = gen_spectra.gen_spectrum(natural_seq, 1, 'b')
+        spectrum_b_2 = gen_spectra.gen_spectrum(natural_seq, 2, 'b')
+        spectrum_y_1 = gen_spectra.gen_spectrum(natural_seq, 1, 'y')
+        spectrum_y_2 = gen_spectra.gen_spectrum(natural_seq, 2, 'y')
+        score_b1, overlap_masses_b1 = find_overlaps(spectrum_b_1, ppm_tolerance, natural.mz_values)
+        score_b2, overlap_masses_b2 = find_overlaps(spectrum_b_2, ppm_tolerance, natural.mz_values)
+        score_y1, overlap_masses_y1 = find_overlaps(spectrum_y_1, ppm_tolerance, natural.mz_values)
+        score_y2, overlap_masses_y2 = find_overlaps(spectrum_y_2, ppm_tolerance, natural.mz_values)
+        
+        converted_masses_b1 = list()
+        converted_masses_b2 = list()
+        converted_masses_y1 = list()
+        converted_masses_y2 = list()
+        for mass in overlap_masses_b1:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 0, 1, natural.precursor_charge)
+            converted_masses_b1.append(converted_mass)
+        for mass in overlap_masses_b2:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 0, 2, natural.precursor_charge)
+            converted_masses_b2.append(converted_mass)
+        for mass in overlap_masses_y1:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 1, 1, natural.precursor_charge)
+            converted_masses_y1.append(converted_mass)
+        for mass in overlap_masses_y2:
+            converted_mass = gen_spectra.convert_ion_to_precursor(mass, 1, 2, natural.precursor_charge)
+            converted_masses_y2.append(converted_mass)
+        
+         #Get the biggest hit for b and y ions 
+        biggest_b_hit = max(converted_masses_b1 + converted_masses_b2) #biggest b hit 
+        biggest_y_hit = max(converted_masses_y1 + converted_masses_y2)
+        
+        #Divide by the precursor mass 
+        nat_prec = natural.precursor_mass
+        biggest_b_hit_nat.append(biggest_b_hit/nat_prec)
+        biggest_y_hit_nat.append(biggest_y_hit/nat_prec)
+        scores_nat.append(score_b1 + score_b2 + score_y1 + score_y2)
+        
+plt1, (ax1, ax2) = plt.subplots(2)
+ax1.hist(biggest_hit_nat, bins=30, color='g', label='natural')
+ax2.hist(biggest_hit_hyb, bins=30, color='r', label='hybrid')
+plt.xlabel("Largest 'good' hit as a proportion of the precursor mass")
+plt.ylabel("Score")
+plt.legend()
+plt.suptitle("Largest 'good' hit as a proportion of the precursor mass")
+plt.savefig("mass_hit_score_normalized")
+       
+#plt1, (ax1, ax2) = plt.subplots(2)
+#ax1.scatter(range(0,len(precursor_score_hyb)), precursor_score_hyb, color='r', label='hybrid')
+#ax2.scatter(range(0,len(precursor_score_nat)), precursor_score_nat, color='g', label='natural')
+#plt.xlabel("Score")
+#plt.ylabel("Frequency")
+#plt.legend()
+#plt.suptitle("Precursor scores for hybrid and naturals")
+#plt.savefig("nat_hyb_scores")
     
-plt1, ax1 = plt.subplots() #color can be red, blue, green, etc
-ax1.scatter(all_overlap_masses_nat, all_yaxis_nat, color = 'b', alpha=.1, label='natural')
-ax1.scatter(all_overlap_masses_hyb, all_yaxis, color = 'r', alpha=.5, label='hybrid')
-plt.xlabel("Mz (/len(seq))")
-plt.ylabel("random jitter")
-plt.title("Mass hits naturals vs hybrids")
-plt.legend()
-plt.savefig("mass_hits_norm")
+#plt1, ax1 = plt.subplots() #color can be red, blue, green, etc
+#ax1.scatter(all_overlap_masses_nat, all_yaxis_nat, color = 'b', alpha=.1, label='natural')
+#ax1.scatter(all_overlap_masses_hyb, all_yaxis, color = 'r', alpha=.5, label='hybrid')
+#plt.xlabel("Mz (/len(seq))")
+#plt.ylabel("random jitter")
+#plt.title("Mass hits naturals vs hybrids")
+#plt.legend()
+#plt.savefig("mass_hits_norm")
 
-plt2, ax1 = plt.subplots() #color can be red, blue, green, etc
-ax1.scatter(all_overlap_masses_nat, scores_nat, color = 'b', alpha=.1, label='natural')
-ax1.scatter(all_overlap_masses_hyb, scores_hyb, color = 'r', alpha=.5, label='hybrid')
-plt.xlabel("Mz (/len(seq))")
-plt.ylabel("Score (/100)")
-plt.axhline(sum(scores_nat)/len(scores_nat))
-plt.title("Score naturals vs hybrids")
-plt.legend()
-plt.savefig("mass_hits_score")
+#plt2, ax1 = plt.subplots() #color can be red, blue, green, etc
+#ax1.scatter(all_overlap_masses_nat, scores_nat, color = 'g', alpha=.1, label='natural')
+#ax1.scatter(all_overlap_masses_hyb, scores_hyb, color = 'r', alpha=0.75, label='hybrid')
+#plt.xlabel("mz")
+#plt.ylabel("Proportion of peaks matched for each spectrum")
+#plt.title("Proportion of peaks matched")
+#plt.legend()
+#plt.savefig("matched_peaks_size")
 
-plt3, ax1 = plt.subplots()
-ax1.hist(scores_nat, color='b', alpha=0.5, label='natural')
-ax1.hist(scores_hyb, color='r', alpha=0.5, label='hybrid')
-plt.xlabel("Score")
-plt.ylabel("Freq.")
-plt.legend()
-plt.savefig("score_hist")
+#plt3, ax1 = plt.subplots()
+#ax1.hist(scores_nat, color='b', alpha=0.5, label='natural')
+#ax1.hist(scores_hyb, color='r', alpha=0.5, label='hybrid')
+#plt.xlabel("Score")
+#plt.ylabel("Freq.")
+#plt.legend()
+#plt.savefig("score_hist")
 
-plt3, ax1 = plt.subplots()
-ax1.hist(all_overlap_masses_nat, color='b', alpha=0.5, label='natural')
-ax1.hist(all_overlap_masses_hyb, color='r', alpha=0.5, label='hybrid')
-plt.xlabel("Score")
-plt.ylabel("Freq.")
-plt.legend()
-plt.axvline(sum(all_overlap_masses_nat)/len(all_overlap_masses_nat))
-plt.savefig("size_hist")
+#plt3, ax1 = plt.subplots()
+#ax1.hist(all_overlap_masses_nat, color='b', alpha=0.5, label='natural')
+#ax1.hist(all_overlap_masses_hyb, color='r', alpha=0.5, label='hybrid')
+#plt.xlabel("Size")
+#plt.ylabel("Freq.")
+#plt.legend()
+#plt.axvline(sum(all_overlap_masses_nat)/len(all_overlap_masses_nat))
+#plt.savefig("size_hist")
+
 
 #2-21 experiments
-percent_matched = list()
-garbage_matches = list()
-missing_matched = list()
-for i,spectrum in enumerate(spectra):
-    #Checking proportion of peaks matched for spectrummill seqs
-    specmill_seq = specmill_seqs[i]
-    score, _ = find_overlaps(specmill_seq, ppm_tolerance, spectrum.mz_values)
-    percent_matched.append(score/peak_filter)
-    missing_matched.append(score/(4*len(specmill_seq)))
-    
-    #Finding # garbage peaks 
-    b_prec, y_prec = gen_spectra.convert_precursor_to_ion(spectrum.precursor_mass, spectrum.precursor_charge)
-    matched_masses_b, matched_masses_y = merge_search.modified_match_masses(spectrum.mz_values, proteins, max_pep_len, ppm_tolerance, b_prec, y_prec)
+#percent_matched = list()
+#garbage_matches = list()
+#missing_matched = list()
+#for i,spectrum in enumerate(spectra):
+#    #Checking proportion of peaks matched for spectrummill seqs
+#    specmill_seq = specmill_seqs[i]
+#    score, _ = find_overlaps(specmill_seq, ppm_tolerance, spectrum.mz_values)
+#    percent_matched.append(score/peak_filter)
+#    missing_matched.append(score/(4*len(specmill_seq)))
+#    
+##    #Finding # garbage peaks 
+#    b_prec, y_prec = gen_spectra.convert_precursor_to_ion(spectrum.precursor_mass, spectrum.precursor_charge)
+#    matched_masses_b, matched_masses_y = merge_search.modified_match_masses(spectrum.mz_values, proteins, max_pep_len, ppm_tolerance, b_prec, y_prec)
     #check how many things match to nothing 
-    count = 0
-    for mass in spectrum.mz_values:
-        if matched_masses_b[mass] == [] and matched_masses_y[mass] == []:
-            count = count + 1  
-    garbage_matches.append(count)
-    
+#    count = 0
+#    for mass in spectrum.mz_values:
+#        if matched_masses_b[mass] == [] and matched_masses_y[mass] == []:
+#            count = count + 1  
+#    garbage_matches.append(count/len(spectrum.mz_values))
+#    
 #plotting the percent of peaks matched 
-plt4, ax1 = plt.subplots() #color can be red, blue, green, etc
-ax1.scatter(range(0,len(spectra)), percent_matched, color = 'b')
-plt.xlabel("range")
-plt.ylabel("percent matched")
-plt.title("Percentage of peaks matched")
-plt.savefig("peak_match")
+#plt4, ax1 = plt.subplots() #color can be red, blue, green, etc
+#ax1.hist(percent_matched, color = 'g', alpha=0.5) #range(0,len(spectra))
+#plt.xlabel("Proportion of peaks matched")
+#plt.ylabel("Frequency")
+#plt.title("Proportion of expected peaks matched for each spectrum")
+#plt.savefig("peak_match")
 
-#plotting the percent of peaks matched 
-plt4, ax1 = plt.subplots() #color can be red, blue, green, etc
-ax1.scatter(range(0,len(spectra)), missing_matched, color = 'b')
-plt.xlabel("range")
-plt.ylabel("percent")
-plt.title("Percentage of good masses in the data")
-plt.savefig("missing_match")
+#plotting proportion of missing expected sequences 
+#plt4, ax1 = plt.subplots() #color can be red, blue, green, etc
+#ax1.hist(missing_matched, color = 'g', alpha=0.5)
+#plt.xlabel("Proportion of possible peaks found")
+#plt.ylabel("Frequency")
+#plt.title("Proportion of possible peaks found for each spectrum")
+#plt.savefig("missing_match")
 
 #Plotting the number of garbage peaks (didn't match to anything)
-plt5, ax1 = plt.subplots() #color can be red, blue, green, etc
-ax1.scatter(range(0,len(spectra)), garbage_matches, color = 'b')
-plt.xlabel("range")
-plt.ylabel("number of garbage peaks")
-plt.title("Number of garbage peaks")
-plt.savefig("garb_peaks")
+#plt5, ax1 = plt.subplots() #color can be red, blue, green, etc
+#ax1.hist(garbage_matches, color = 'g', alpha=0.5)
+#plt.xlabel("Proportion of 'noisy' peaks")
+#plt.ylabel("Frequency")
+#plt.title("Proportion of noisy peaks for each spectrum")
+#plt.savefig("garb_peaks")
