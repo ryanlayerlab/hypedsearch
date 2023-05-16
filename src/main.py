d20269aa93482803e5ec02b14ad6ed6a204c29fc
jamesdixon <jamessdixon@gmail.com>
2023-05-16 12:42:45 -0600

    UIpdate tp get running on my machine

--- src/main.py
+++ src/main.py
@@ -68,7 +68,6 @@ def main(args: object) -> None:
     output_dir = arguments['output_dir']
     print('\nFinished search. Writting results to {}...'.format(output_dir))
     summary.generate_to_txt(matched_spectras, arguments["spectra_files"], output_dir)
-    # summary.generate(matched_spectras, output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tool for identifying proteins, both hybrid and non hybrid from MS/MS data')
