a70d6808bd69b1d002bd17fbe6301e99d74db339
jamesdixon <jamessdixon@gmail.com>
2023-10-28 18:07:32 -0600

    removed .cpp module runner

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import argparse
 import sys
 import distutils
 from src.constants.objects import Database
-import src.constants.utils, runner, database
+import src.constants.utils, runner, src.computational_pipeline.database
 from config_loader import Config
 from postprocessing import summary, review
 
@@ -21,7 +21,7 @@ def get_spectra_files(spectra_folder):
     return spectra_files
 
 def get_database_file(database_file_path):
-    return database.build(database_file_path)
+    return src.computational_pipeline.database.build(database_file_path)
 
 def set_args(args) -> dict:
     use_params = string_to_bool(args.config)
@@ -45,17 +45,17 @@ def set_args(args) -> dict:
     num_natives = args.num_natives if not use_params else config['top_natives']
     debug = args.debug if not use_params else config['debug']
 
-    if not utils.is_dir(spectra_folder):
+    if not src.constants.utils.is_dir(spectra_folder):
         print(f'Error: {spectra_folder} is not a real path. Path to directory with spectra files is necessary.')
         sys.exit(0)
-    if not utils.is_fasta(database_file_path) or not utils.is_file(database_file_path):
+    if not src.constants.utils.is_fasta(database_file_path) or not src.constants.utils.is_file(database_file_path):
         print(f'Error: {database_file_path} is not a valid .fasta file. .fasta file needed.')
         sys.exit(0)
 
     spectra_files = get_spectra_files(spectra_folder)
     database_file = get_database_file(database_file_path)
-    output_dir = utils.make_valid_dir_string(output_dir)
-    utils.make_dir(output_dir)
+    output_dir = src.constants.utils.make_valid_dir_string(output_dir)
+    src.constants.utils.make_dir(output_dir)
 
     return {'spectra_files': spectra_files,'database_file': database_file,'output_dir': output_dir,
         'min_peptide_len': min_peptide_len,'max_peptide_len': max_peptide_len,'tolerance': ppm_tolerance,
@@ -86,17 +86,13 @@ if __name__ == '__main__':
     parser.add_argument('--precursor-tolerance', dest='precursor_tolerance', type=float, default=1, help='ppm tolerance to accept when matching precursor masses. Default=10')
     parser.add_argument('--peak-filter', dest='peak_filter', type=int, default=0, help='The number of peaks to take from a spectrum. The most abundant peaks will be taken. Leave blank if you want no filter or to use relative abundance filter. Defualt=0')
     parser.add_argument('--abundance-filter', dest='rel_abund_filter', type=float, default=0.0, help='Take only peaks from a spectrum where the abundance of the peak is >= the percentage give. Leave blank if you want no filter or to use peak filter. Default=0.0')
-
     parser.add_argument('--digest-left', dest='digest_left', nargs='*', default=[''], type = str, help='The Amino Acid for which the digest cuts left of. Default=None')
     parser.add_argument('--digest-right', dest='digest_right', nargs='*', default=[''], type = str, help='The Amino Acid for which the digest cuts right of. Default=None')
-
     parser.add_argument('--verbose', dest='verbose', type=lambda x:bool(distutils.util.strtobool(x)))
     parser.add_argument('--cores', dest='cores', type=int, default=1, help='The number of cores allowed to use when searching. Uses at least 1 and at most the number of available cores. Default=1')
     parser.add_argument('--new-database', dest='new_db', type=lambda x:bool(distutils.util.strtobool(x)))
     parser.add_argument('--num-hybrids', dest='num_hybrids', type=int, default=5, help='The number of hybrid alignments to keep per spectrum. Default=5')
     parser.add_argument('--num-natives', dest='num_natives', type=int, default=5, help='The number of native alignments to keep per spectrum. Default=5')
-
     parser.add_argument('--debug', dest='debug', type=bool, default=False, help='The number of alignments to keep per spectrum. Default=5')
     args = parser.parse_args()
-
     main(args)
