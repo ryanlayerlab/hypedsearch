ea3215b95443a53cc09e2c511dfc108468b6e40d
jamesdixon <jamessdixon@gmail.com>
2024-01-15 06:39:17 -1000

    changed function names

--- src/computational_pipeline/gen_spectra.py
+++ src/computational_pipeline/gen_spectra.py
@@ -2,7 +2,7 @@
 from lookups.constants import AMINO_ACIDS, WATER_MASS, SINGLY_CHARGED_B_BASE, SINGLY_CHARGED_Y_BASE, DOUBLY_CHARGED_B_BASE, DOUBLY_CHARGED_Y_BASE, INTEGER_ORDERED_AMINO_ACIDS, PROTON_MASS, AMMONIUM
 import numpy as np
 
-def b_ions(sequence: str, charge: int = None): 
+def get_b_ions(sequence: str, charge: int = None): 
     masses = []
     length = len(sequence)
     if charge is None or charge == 1:
@@ -17,7 +17,7 @@ def b_ions(sequence: str, charge: int = None):
             masses.append(total/2)
     return masses
 
-def y_ions(sequence: str, charge: int = None): 
+def get_y_ions(sequence: str, charge: int = None): 
     masses = []
     length = len(sequence)
     if charge is None or charge == 1:
@@ -42,9 +42,9 @@ def calc_masses(sequence: str, charge: int =None, ion: str = None):
     pre_mz_charge = 2 if charge is None else charge
     pre_mz = (total+pre_mz_charge*PROTON_MASS)/pre_mz_charge
     if ion is None or ion == 'b': 
-        masses += b_ions(sequence, charge=charge)
+        masses += get_b_ions(sequence, charge=charge)
     if ion is None or ion == 'y': 
-        masses += y_ions(sequence, charge=charge)
+        masses += get_y_ions(sequence, charge=charge)
     return masses, pre_mz
 
 def calc_masses_no_water(sequence: str, charge: int =None, ion: str = None):
@@ -56,9 +56,9 @@ def calc_masses_no_water(sequence: str, charge: int =None, ion: str = None):
     pre_mz_charge = 2 if charge is None else charge
     pre_mz = (total+pre_mz_charge*PROTON_MASS)/pre_mz_charge
     if ion is None or ion == 'b': 
-        masses += b_ions(sequence, charge=charge)
+        masses += get_b_ions(sequence, charge=charge)
     if ion is None or ion == 'y': 
-        masses += y_ions(sequence, charge=charge)
+        masses += get_y_ions(sequence, charge=charge)
     return masses, pre_mz
 
 def calc_masses_no_ammonium(sequence: str, charge: int =None, ion: str = None):
@@ -71,12 +71,12 @@ def calc_masses_no_ammonium(sequence: str, charge: int =None, ion: str = None):
     pre_mz_charge = 2 if charge is None else charge
     pre_mz = (total+pre_mz_charge*PROTON_MASS)/pre_mz_charge
     if ion is None or ion == 'b': 
-        masses += b_ions(sequence, charge=charge)
+        masses += get_b_ions(sequence, charge=charge)
     if ion is None or ion == 'y': 
-        masses += y_ions(sequence, charge=charge)
+        masses += get_y_ions(sequence, charge=charge)
     return masses, pre_mz
 
-def max_mass(seqeunce: str, ion: str, charge: int):
+def get_max_mass(seqeunce: str, ion: str, charge: int):
     if ion == 'y':
         total = SINGLY_CHARGED_Y_BASE if charge == 1 else DOUBLY_CHARGED_Y_BASE
         total += sum([AMINO_ACIDS[aa] for aa in seqeunce])
@@ -149,17 +149,17 @@ def calc_precursor_as_disjoint(b_mass, y_mass, b_charge, y_charge, precursor_cha
     precursor = convert_raw_to_precursor(total_sequence_mass, precursor_charge)
     return precursor
 
-def gen_spectrum(sequence: str, charge: int = None, ion: str = None):
+def generate_spectrum(sequence: str, charge: int = None, ion: str = None):
     this_entry = {}
     masses, pre_mz = calc_masses(sequence, charge=charge, ion=ion)
     this_entry['spectrum'] = masses
     this_entry['precursor_mass'] = pre_mz
     return this_entry
 
-def gen_spectra(sequences: list, charge=None, ion=None):
-    return [gen_spectrum(seq, charge=charge, ion=ion) for seq in sequences]
+def generate_spectra(sequences: list, charge=None, ion=None):
+    return [generate_spectrum(seq, charge=charge, ion=ion) for seq in sequences]
 
-def gen_min_ordering(sequence: str):
+def generate_min_ordering(sequence: str):
     middle = [np.int8(INTEGER_ORDERED_AMINO_ACIDS[aa]) for aa in sequence if aa in INTEGER_ORDERED_AMINO_ACIDS]
     if len(middle) == 0:
         return []
