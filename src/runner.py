5a46654c7464bae154e60aa2905d79cea89cc480
jamesdixon <jamessdixon@gmail.com>
2024-05-29 17:57:39 -0600

    changes

--- src/runner.py
+++ src/runner.py
@@ -51,8 +51,9 @@ def run(args: dict) -> dict:
     number_hybrids=args['number_hybrids']
     number_natives=args['number_natives']
     target_seq = args['target_seq']
-    aligned_spectras = computational_pipeline.identification.get_aligned_spectrums(spectrums,built_database,max_peptide_length,ppm_tolerance,precursor_tolerance,number_hybrids,number_natives,target_seq)  
-    output_file_name = get_output_file_name(args['spectra_file_paths']) 
-    output_folder_path=args['output_folder_path']
+    aligned_spectrums = computational_pipeline.identification.get_aligned_spectrums(spectrums,built_database,max_peptide_length,ppm_tolerance,precursor_tolerance,number_hybrids,number_natives,target_seq)  
+    print(f"Number of aligned spectrums: {len(aligned_spectrums)}")
+    # output_file_name = get_output_file_name(args['spectra_file_paths']) 
+    # output_folder_path=args['output_folder_path']
     # write_aligned_spectras_to_disk(aligned_spectras, output_folder_path, output_file_name)
     
