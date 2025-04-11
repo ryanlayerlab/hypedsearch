c68e312bddc4b7a959eb8812716ce85446f6068c
Erik Johnson <airik.k.johnson@gmail.com>
2025-04-11 15:43:40 -0600

    updated fiji scripts

--- slurm/run_hs_on_all_mzmls/run.py
+++ slurm/run_hs_on_all_mzmls/run.py
@@ -7,14 +7,28 @@ sys.path.append(str(repo_dir))
 
 import subprocess
 
+from src.comet_utils import CometPSM
 from src.constants import SPECTRA_DIR
 
-mzml_paths = list(SPECTRA_DIR.glob("*.mzML"))
-# mzml_paths = mzml_paths[:2]
-for mzml in mzml_paths:
+# mzml_paths = list(SPECTRA_DIR.glob("*.mzML"))
+
+# mzml_paths = [SPECTRA_DIR / "BMEM_AspN_Fxn5.mzML"]  # for testing
+
+df = CometPSM.from_txt(
+    file_path=Path("results/new_fasta/BMEM_AspN_Fxn5.txt"), as_df=True
+)
+scans = list(df["scan"].unique())
+print(f"Number of total scans = {len(scans)}")
+
+mzml = (SPECTRA_DIR / "BMEM_AspN_Fxn5.mzML").absolute()
+for scan in scans[0:1000]:
+    # for scan in scans[1000:2000]:
+    # for scan in scans[2000:3000]:
+    # for scan in scans[3000:]:
     cmd = [
         "sbatch",
         "slurm/run_hs_on_all_mzmls/run_on_one_mzml.sbatch",
         mzml,
+        scan,
     ]
     subprocess.Popen(cmd)  # Runs the command without waiting for it to finish
